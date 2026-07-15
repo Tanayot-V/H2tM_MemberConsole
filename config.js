571f622ceca2420eff682f0363647e3744bb324f
Earth <tanayot.v@gmail.com>
2026-07-15 16:29:58 +0700

    1.0.7_Change Config to Prod

--- config.js
+++ config.js
@@ -3,6 +3,6 @@
 // page anyway (view-source/network tab), so hiding them from git buys
 // nothing once deployed to GitHub Pages.
 window.APP_CONFIG = {
-  LIFF_ID: "2010611262-7rSUDS90", // e.g. "1234567890-AbCdEfGh"
+  LIFF_ID: "2010718190-FvhszJY1", // e.g. "1234567890-AbCdEfGh"
   GAS_WEB_APP_URL: "https://script.google.com/macros/s/AKfycbwwXDF4rhRadFmioaFBOeIiqoCfq3bgzhGTopLuKAvbKoduCb2T0szoHXsiwDTRM7lN/exec",
 };
